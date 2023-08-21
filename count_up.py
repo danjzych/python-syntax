914e98723d8fb2fa395f79ad7da06ee5ccd70361
Daniel Zych <danjzych@gmail.com>
2023-08-21 13:46:36 -0500

    finished count_up

--- count_up.py
+++ count_up.py
@@ -1,18 +1,9 @@
 def count_up(start, stop):
-    """Print all numbers from start up to and including stop.
-
-    For example:
-
-        count_up(5, 7)
-
-   should print:
-
-        5
-        6
-        7
-    """
-
-    # YOUR CODE HERE
+    '''This function counts up'''
+    for n in range(start, stop+1):
+        print(n)
 
 
-count_up(5, 7)        
+
+
+count_up(5, 7)
